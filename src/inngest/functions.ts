4cce2c073b1b45d02a5cd26b04400bdb5f237767
Dr. Suren Movsisyan <suren@movsisyan.dk>
2026-09-01 10:41:21 +0400

    04: background jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,16 @@
+import { generateText } from "ai";
+import { inngest } from "./client";
+import { anthropic } from "@ai-sdk/anthropic";
+
+export const demoGenerate = inngest.createFunction(
+  { id: "demo-generate" },
+  { event: "demo/generate" },
+  async ({ step }) => {
+    await step.run("generate-text", async () => {
+     return  await generateText({
+    model: anthropic('claude-haiku-4-5'), 
+    prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+  });
+    })
+  },
+);
