71227eaaeb75ec5b62a4c7fbdac1fa9f8438b25f
Dr. Suren Movsisyan <suren@movsisyan.dk>
2026-09-03 19:11:47 +0400

    06: error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1f8c8e36bcc80e7eea538c7f16899a4b@o4512016771645440.ingest.de.sentry.io/4512016777019472",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+      integrations: [
+        Sentry.vercelAIIntegration,
+        Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+      ],
+  },
+});
